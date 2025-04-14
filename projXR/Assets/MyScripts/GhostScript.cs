4d6b6318e01f28b2c9a4c11927478acaeecd0e1b
ilacftemp <ilachafin@gmail.com>
2025-04-14 02:11:30 -0300

    jogo pronto

--- projXR/Assets/MyScripts/GhostScript.cs
+++ projXR/Assets/MyScripts/GhostScript.cs
@@ -5,7 +5,8 @@ using UnityEngine.AI;
 
 public class GhostScript : MonoBehaviour
 {
-
+    public float eatDistance = 0.2f;
+    public Animator animator;
     public NavMeshAgent agent;
     public float speed = 1f;
 
@@ -15,11 +16,53 @@ public class GhostScript : MonoBehaviour
         
     }
 
+    public GameObject getClosestOrb(){
+        GameObject closes = null;
+        float minDistance = Mathf.Infinity;
+
+        List<GameObject> orbs = OrbSpawner.instance.spawnedOrbs;
+
+        foreach(Var item in orbs){
+            Vector3 ghostPosition = transform.position;
+            ghostPosition.y = 0;
+
+            Vector3 orbPosition = item.transform.position;
+            orbPosition.y = 0;
+
+            float d = Vector3.Distance(transform.position, item.transform.position);
+            if(d < minDistance){
+                minDistance = d;
+                closest = item;
+            }
+        }
+
+        if(minDistance < eatDistance){
+            OrbSpawner.instance.DestroyOrb(closest);
+        }
+
+        return closest;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = Camera.main.transform.position;
-        agent.SetDestination(targetPosition);
-        agent.speed = speed;
+        if(!agent.enabled) return;
+
+        GameObject closest = getClosestOrb();
+
+        if(closest){
+            Vector3 targetPosition = closest.transform.position;
+            agent.SetDestination(targetPosition);
+            agent.speed = speed;
+        }
+    }
+
+    public void Kill(){
+        agent.enabled = false;
+        animator.SetTrigger("Death");
+    }
+
+    public void Destroy(){
+        Destroy(gameObject);
     }
 }
